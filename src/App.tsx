db0aff9f453310a6264ec49168c70dfc0baa1f7e
Hugo <quperstudio@gmail.com>
2025-10-25 16:37:44 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import ProtectedRoute from './components/ProtectedRoute';
-import { Toaster } from './components/ui/toaster'; // <--- CAMBIO AQUÍ: Importación de Sonner eliminada
+import { Toaster } from './components/ui/toaster';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { queryClient } from './lib/queryClient';
@@ -53,11 +53,9 @@ function App() {
                   </ProtectedRoute>
                 }
               />
-              {/* Añade más rutas aquí */}
             </Routes>
           </BrowserRouter>
           <Toaster /> 
-          {/* <--- CAMBIO AQUÍ: <SonnerToaster ... /> fue eliminado */}
         </AuthProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>

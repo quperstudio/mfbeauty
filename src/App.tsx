3fa9b86ca713b02c56ebde0fcad41bc9cf4bb96a
Hugo <quperstudio@gmail.com>
2025-10-26 16:13:52 -0700

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
                 }
               />
               <Route
-                path="/calendario"
+                path="/calendar"
                 element={
                   <ProtectedRoute>
                     <AppLayout>

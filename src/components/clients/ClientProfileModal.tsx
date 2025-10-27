41a6a00667e06dc4c0302ee09a850f37b0123a08
Hugo <quperstudio@gmail.com>
2025-10-26 22:21:30 -0700

    Updated ClientProfileModal.tsx

--- src/components/clients/ClientProfileModal.tsx
+++ src/components/clients/ClientProfileModal.tsx
@@ -453,7 +453,18 @@ export default function ClientProfileModal({ isOpen, onClose, clientId, onEdit }
   						</div>
   					</ScrollArea>
 
-
+  					{/* --- Layout Desktop --- */}
+  					<div className="hidden md:grid md:grid-cols-10 gap-6 h-full p-6 pt-0">
+  						{/* Col Izq (30%) */}
+  						<ScrollArea className="md:col-span-3 h-full">
+  							<LeftColumnContent />
+  						</ScrollArea>
+  						{/* Col Der (70%) */}
+  						<ScrollArea className="md:col-span-7 h-full">
+  							<RightColumnContent />
+  						</ScrollArea>
+  					</div>
+  				</div>
   
   			</DialogContent>
   		</TooltipProvider>
